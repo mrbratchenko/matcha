e4f6d238da43f123dd4180d5658f7b74986f7865
Sergii BRATCHENKO <sbratche@e1r9p15.unit.ua>
2019-04-22 20:45:30 +0300

    killed mongoSchemas

--- mongoSchemas/User.js
+++
@@ -1,9 +0,0 @@
-// // module.exports = db.createCollection("users", {
-// //   validator: {
-// //     $jsonSchema: {
-// //       bsonType: "object",
-// //       required: ["name", "email", "password"],
-// //       additionalProperties: false,
-// //       properties: {
-// //         name: {
-// //           bsonType: "string",
